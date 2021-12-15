318a389d37c3ae35eeae640a107820c30ffe4c77
Nero <andrewpajva@gmail.com>
2021-12-15 23:09:22 +1100

    The Base Game

---
+++ PatternGuessGame/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿
+namespace PatternGuessGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTL = new System.Windows.Forms.Button();
+            this.btnTM = new System.Windows.Forms.Button();
+            this.btnTR = new System.Windows.Forms.Button();
+            this.btnMR = new System.Windows.Forms.Button();
+            this.btnMM = new System.Windows.Forms.Button();
+            this.btnML = new System.Windows.Forms.Button();
+            this.btnBR = new System.Windows.Forms.Button();
+            this.btnBM = new System.Windows.Forms.Button();
+            this.btnBL = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblLvl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTL
+            // 
+            this.btnTL.BackColor = System.Drawing.Color.White;
+            this.btnTL.Location = new System.Drawing.Point(13, 14);
+            this.btnTL.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnTL.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnTL.Name = "btnTL";
+            this.btnTL.Padding = new System.Windows.Forms.Padding(10);
+            this.btnTL.Size = new System.Drawing.Size(70, 70);
+            this.btnTL.TabIndex = 0;
+            this.btnTL.UseVisualStyleBackColor = false;
+            this.btnTL.Click += new System.EventHandler(this.btnTL_Click);
+            // 
+            // btnTM
+            // 
+            this.btnTM.BackColor = System.Drawing.Color.White;
+            this.btnTM.Location = new System.Drawing.Point(89, 14);
+            this.btnTM.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnTM.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnTM.Name = "btnTM";
+            this.btnTM.Padding = new System.Windows.Forms.Padding(10);
+            this.btnTM.Size = new System.Drawing.Size(70, 70);
+            this.btnTM.TabIndex = 1;
+            this.btnTM.UseVisualStyleBackColor = false;
+            this.btnTM.Click += new System.EventHandler(this.btnTM_Click);
+            // 
+            // btnTR
+            // 
+            this.btnTR.BackColor = System.Drawing.Color.White;
+            this.btnTR.Location = new System.Drawing.Point(165, 14);
+            this.btnTR.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnTR.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnTR.Name = "btnTR";
+            this.btnTR.Padding = new System.Windows.Forms.Padding(10);
+            this.btnTR.Size = new System.Drawing.Size(70, 70);
+            this.btnTR.TabIndex = 2;
+            this.btnTR.UseVisualStyleBackColor = false;
+            this.btnTR.Click += new System.EventHandler(this.btnTR_Click);
+            // 
+            // btnMR
+            // 
+            this.btnMR.BackColor = System.Drawing.Color.White;
+            this.btnMR.Location = new System.Drawing.Point(165, 90);
+            this.btnMR.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnMR.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnMR.Name = "btnMR";
+            this.btnMR.Padding = new System.Windows.Forms.Padding(10);
+            this.btnMR.Size = new System.Drawing.Size(70, 70);
+            this.btnMR.TabIndex = 5;
+            this.btnMR.UseVisualStyleBackColor = false;
+            this.btnMR.Click += new System.EventHandler(this.btnMR_Click);
+            // 
+            // btnMM
+            // 
+            this.btnMM.BackColor = System.Drawing.Color.White;
+            this.btnMM.Location = new System.Drawing.Point(89, 90);
+            this.btnMM.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnMM.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnMM.Name = "btnMM";
+            this.btnMM.Padding = new System.Windows.Forms.Padding(10);
+            this.btnMM.Size = new System.Drawing.Size(70, 70);
+            this.btnMM.TabIndex = 4;
+            this.btnMM.UseVisualStyleBackColor = false;
+            this.btnMM.Click += new System.EventHandler(this.btnMM_Click);
+            // 
+            // btnML
+            // 
+            this.btnML.BackColor = System.Drawing.Color.White;
+            this.btnML.Location = new System.Drawing.Point(13, 90);
+            this.btnML.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnML.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnML.Name = "btnML";
+            this.btnML.Padding = new System.Windows.Forms.Padding(10);
+            this.btnML.Size = new System.Drawing.Size(70, 70);
+            this.btnML.TabIndex = 3;
+            this.btnML.UseVisualStyleBackColor = false;
+            this.btnML.Click += new System.EventHandler(this.btnML_Click);
+            // 
+            // btnBR
+            // 
+            this.btnBR.BackColor = System.Drawing.Color.White;
+            this.btnBR.Location = new System.Drawing.Point(165, 166);
+            this.btnBR.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnBR.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnBR.Name = "btnBR";
+            this.btnBR.Padding = new System.Windows.Forms.Padding(10);
+            this.btnBR.Size = new System.Drawing.Size(70, 70);
+            this.btnBR.TabIndex = 8;
+            this.btnBR.UseVisualStyleBackColor = false;
+            this.btnBR.Click += new System.EventHandler(this.btnBR_Click);
+            // 
+            // btnBM
+            // 
+            this.btnBM.BackColor = System.Drawing.Color.White;
+            this.btnBM.Location = new System.Drawing.Point(89, 166);
+            this.btnBM.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnBM.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnBM.Name = "btnBM";
+            this.btnBM.Padding = new System.Windows.Forms.Padding(10);
+            this.btnBM.Size = new System.Drawing.Size(70, 70);
+            this.btnBM.TabIndex = 7;
+            this.btnBM.UseVisualStyleBackColor = false;
+            this.btnBM.Click += new System.EventHandler(this.btnBM_Click);
+            // 
+            // btnBL
+            // 
+            this.btnBL.BackColor = System.Drawing.Color.White;
+            this.btnBL.Location = new System.Drawing.Point(13, 166);
+            this.btnBL.MaximumSize = new System.Drawing.Size(70, 70);
+            this.btnBL.MinimumSize = new System.Drawing.Size(70, 70);
+            this.btnBL.Name = "btnBL";
+            this.btnBL.Padding = new System.Windows.Forms.Padding(10);
+            this.btnBL.Size = new System.Drawing.Size(70, 70);
+            this.btnBL.TabIndex = 6;
+            this.btnBL.UseVisualStyleBackColor = false;
+            this.btnBL.Click += new System.EventHandler(this.btnBL_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnMM);
+            this.panel1.Controls.Add(this.btnBR);
+            this.panel1.Controls.Add(this.btnTL);
+            this.panel1.Controls.Add(this.btnBM);
+            this.panel1.Controls.Add(this.btnTM);
+            this.panel1.Controls.Add(this.btnBL);
+            this.panel1.Controls.Add(this.btnTR);
+            this.panel1.Controls.Add(this.btnMR);
+            this.panel1.Controls.Add(this.btnML);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(249, 250);
+            this.panel1.TabIndex = 9;
+            // 
+            // lblLvl
+            // 
+            this.lblLvl.AutoSize = true;
+            this.lblLvl.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblLvl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblLvl.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLvl.Location = new System.Drawing.Point(267, 26);
+            this.lblLvl.MinimumSize = new System.Drawing.Size(70, 70);
+            this.lblLvl.Name = "lblLvl";
+            this.lblLvl.Size = new System.Drawing.Size(70, 70);
+            this.lblLvl.TabIndex = 10;
+            this.lblLvl.Text = "1";
+            this.lblLvl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(267, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Current Lv.";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(267, 103);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(70, 32);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 276);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblLvl);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Pattern Test";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTL;
+        private System.Windows.Forms.Button btnTM;
+        private System.Windows.Forms.Button btnTR;
+        private System.Windows.Forms.Button btnMR;
+        private System.Windows.Forms.Button btnMM;
+        private System.Windows.Forms.Button btnML;
+        private System.Windows.Forms.Button btnBR;
+        private System.Windows.Forms.Button btnBM;
+        private System.Windows.Forms.Button btnBL;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLvl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ PatternGuessGame/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PatternGuessGame
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttons.Add(btnTL);
+            buttons.Add(btnTM);
+            buttons.Add(btnTR);
+            buttons.Add(btnML);
+            buttons.Add(btnMM);
+            buttons.Add(btnMR);
+            buttons.Add(btnBL);
+            buttons.Add(btnBM);
+            buttons.Add(btnBR);
+            
+        }
+        //Global Variables
+        List<Button> buttons = new List<Button>();
+        int intLv = 1; //for number of consecutive clicks
+        List<int> clickSequence = new List<int>();//list for the click order
+        List<int> clickOrder = new List<int>();
+        int clickCount = 0;
+
+        private void createSequence()
+        {
+            clickSequence.Clear();
+            Random rng = new Random();
+            //repeats to the current lv
+            for (int i = 0; i < intLv; i++)
+            {   //creates a random click             
+                
+                clickSequence.Add(rng.Next(0, 8));
+            }
+            
+        }
+
+        private void EnableDisable(bool x)
+        {
+            foreach (var item in buttons)
+            {
+                item.Enabled = x;
+            }
+        }
+
+        private void showSequence()
+        {
+            EnableDisable(false);
+            foreach (var item in clickSequence)
+            {                
+                    buttons[item].BackColor = Color.Red;
+                    Thread.Sleep(1000);
+                buttons[item].BackColor = Color.White;          
+                
+            }
+            EnableDisable(true);
+        }
+
+        private void Failed()
+        {
+            MessageBox.Show($"Ha Suffer\nCurrentLevel: {intLv}");
+            intLv = 1;
+            Application.Restart();
+        }
+
+        private void CheckSequence()
+        {
+            for (int i = 0; i < clickSequence.Count-1; i++)
+            {
+                if (clickSequence[i] != clickOrder[i])
+                {
+                    Failed();
+                    return;
+                }
+            }
+            intLv++;
+            clickOrder.Clear();
+            clickCount = 0;
+            lblLvl.Text = $"{intLv}";
+            createSequence();
+            Thread t = new Thread(new ThreadStart(showSequence));
+            t.Start();
+        }
+
+       //button clicks
+
+        private void btnTL_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(0);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnTM_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(1);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnTR_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(2);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnML_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(3);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnMM_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(4);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnMR_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(5);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnBL_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(6);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnBM_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(7);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+
+        private void btnBR_Click(object sender, EventArgs e)
+        {
+            clickCount++;
+            clickOrder.Add(8);
+            if (clickCount == intLv)
+            {
+                CheckSequence();
+            }
+        }
+        
+
+        //Start button click
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            createSequence();
+            Thread t = new Thread(new ThreadStart(showSequence));
+            t.Start();
+            
+        }        
+    }
+}
